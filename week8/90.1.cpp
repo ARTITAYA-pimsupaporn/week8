eb7d7bfa98d286d1d5dc26bcb0af257fc351521e
DELL <DELL@DESKTOP-7K6E1DD>
2021-09-20 16:52:26 +0700

    Add project files.

---
+++ week8/90.1.cpp
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<math.h>
+//จงเขียนฟังก์ชัน sqrt โดยไม่ใช้ไลบรารี math.h(Level 5)
+
+double sqroot(double s) {
+	double x0 = 600;			// มันเป็น theory
+	double x1 = 0.0;
+	double precision = .0001;
+	double delta = 1;
+	while (delta > precision) {
+		x1 = .5 * (x0 + s / x0);
+		delta = fabs(x1 - x0);
+		printf("x0 = %.2lf, x1 = %.2lf, delta = %.2lf\n", x0, x1, delta);
+		x0 = x1;
+	}
+
+	return x1;
+}
+int main() {
+
+	int s;
+	scanf("%d", &s);
+	double x = sqroot(s);
+	printf("\nX = %.2lf , X*X = %.2lf\n", x, x * x);
+
+
+	return 0;
+}
---
+++ week8/90.2.cpp
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//จงเขียนฟังก์ชัน sqrt โดยไม่ใช้ไลบรารี math.h(Level 5)
+float x, temp = 0;
+void sqroot(float st)
+{
+    if (st != temp)
+    {
+        temp = st;
+        st = ((x / temp) + temp) / 2;
+        sqroot(st);
+    }
+    else
+    {
+        printf("%.2f", st);
+    }
+}
+int main()
+{
+    scanf("%f", &x);
+    sqroot(x / 2);
+    return 0;
+}
